bd68288f2eb96df32639e26e49e291cf87b3c344
sanjelprashid <168109770+sanjelprashid@users.noreply.github.com>
2025-01-07 12:07:29 +0545

    Add files via upload

---
+++ LogInAndSingUp.java
@@ -0,0 +1,19 @@
+
+package com.model;
+
+import com.view.Login;
+
+/**
+ *
+ * @author sanje
+ */
+public class LogInAndSingUp {
+    public static void main(String[] args) {
+        
+        
+       Login LoginFrame = new Login();
+       LoginFrame.setVisible(true);
+       LoginFrame.pack();
+       LoginFrame.setLocationRelativeTo(null);
+    }
+}
